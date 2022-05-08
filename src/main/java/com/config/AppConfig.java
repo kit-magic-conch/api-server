fed32dc5ceb3451bbe287c6c61316c3b9ea6dbd8
gh1224 <rlarkgus0@naver.com>
2022-05-09 00:49:04 +0900

    refactor: @EnableJpaAuditing Application -> AppConfig 이동
    @WebMvcTest 시 JPA 관련 bean들에 대한 의존성 제거

---
+++ src/main/java/com/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AppConfig {
+}
