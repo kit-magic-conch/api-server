bc3ff42381e4f715889d6a531cbaffc758050fc8
gh1224 <rlarkgus0@naver.com>
2022-05-20 13:02:50 +0900

    fix: axios 로그아웃 시 리다이렉트 문제 해결
    axios 사용하여 로그아웃 시 로그인 페이지로 리다이렉트하는 핸들러가 LogoutSuccessHandler로서 작동
    -> HttpStatusReturningLogoutSuccessHandler 강제

--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
@@ -35,7 +37,8 @@ public class SecurityConfig {
 
         http
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/session", "DELETE"));
+                .logoutRequestMatcher(new AntPathRequestMatcher("/session", "DELETE"))
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT));
 
         http
                 .httpBasic();
